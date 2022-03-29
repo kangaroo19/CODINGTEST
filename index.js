34a0ee46ab57ebe32e65c14cc784e657a7c2a52b
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-30 01:34:58 +0900

    123

---
+++ index.js
@@ -0,0 +1,10 @@
+//백준에 업로드 시 readFileSync 안의 값을 '/dev/stdin'으로
+//한줄에 입력시 split(' ')
+//두줄에 입력 split('\n')
+let input = require('fs').readFileSync('example.txt').toString().split('\n');
+
+//배열입력 ->let numbers=input[0].split(' ').map(x=>parseInt(x));
+
+
+
+

1e94feca4fe05f853696ae9f76de43b185f0207d
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-04-02 01:14:37 +0900

    10809

--- backjoon/10809.js
+++ backjoon/10809.js
@@ -3,20 +3,9 @@
 //두줄에 입력 split('\n')
 let input = require('fs').readFileSync('example.txt').toString().split(' ');
 
-
 const str=input.join();//배열을 문자열로 변환
 const array=[];
-
-for(let i=0;i<26;i++){
-    array[i]=-1;
+for(let i=97;i<123;i++){
+    array.push(str.indexOf(String.fromCharCode(i)));
 }
-
-for(let i=0;i<str.length;i++){
-    
-}
-
-console.log(str.indexOf('o'));
-
-
-
-
+console.log(array.join(" "));
--- index.js
+++ index.js
@@ -5,24 +5,9 @@ let input = require('fs').readFileSync('example.txt').toString().split(' ');
 
 //배열입력 ->let numbers=input[0].split(' ').map(x=>parseInt(x));
 const str=input.join();//배열을 문자열로 변환
-let answer=[];
-for(let i=0;i<26;i++){  //26(알파벳개수)크기의 answer배열을 0으로 채움
-    answer[i]=0;
-}
-
-for(let i=97;i<=122;i++){
-    for(let j=97;j<=122;j++){
-        if(str.charCodeAt(j-97)==i){ //charcodeat=>str(입력)문자열의 인덱스 값에 대한 유니코드 반환
-            answer[i-97]++;
-        }
-    }
-}
-for(let i=0;i<26;i++){  //26(알파벳개수)크기의 answer배열을 0으로 채움
-    if(answer[i]==0){
-        answer[i]=-1;
-    }
-}
-console.log(answer);
+// console.log(String.fromCharCode(a)); //유니코드 문자로 변환
+//console.log(str.charCodeAt(0)); //인덱스 번호에 해당하는 문자 유니코드 변환
+console.log(str.charCodeAt(str[0]));
 
 
 

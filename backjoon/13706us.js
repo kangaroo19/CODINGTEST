57d1b4df5f0a27e6e562e2bcac4bfc88fff28629
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-05-23 21:17:48 +0900

    123

--- backjoon/13706us.js
+++ backjoon/13706us.js
@@ -14,5 +14,15 @@ const array=[]
 // console.log(array[mid])
 //시간초과
 //-----------------------------------------
+let left=0
+let right=n;
+function a(n){
+    while(left<=right){
+        let mid=Math.floor((left+right)/2)
+        if(mid**2<=n) left=mid+1
+        else right=mid-1
+        if(mid**2==n)  return mid
+    }  
+}
 
-
+console.log(a(n))

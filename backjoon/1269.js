a6b047a92f2e7205eb4895e972816fb31e8c95da
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-05-27 16:15:15 +0900

    1269

--- backjoon/1269.js
+++ backjoon/1269.js
@@ -1,6 +1,20 @@
 const fs=require('fs');
 const input=fs.readFileSync("example.txt").toString().trim().split('\n');
 const nums = input.map(v => v.split(' ').map(x => +x));
+const n=nums[0].shift()
+const m=nums[0].shift()
+const array1=nums[1].splice(0,n)
+const array2=nums[2].splice(0,m)
 
-const n=Number(nums[0].shift)
-const m=Number(nums[0].shift)
+let set1=new Set(array1)
+let set2=new Set(array2)
+
+function a(set1,set2){
+    let difference=new Set(set1)
+    set2.forEach(e=>{
+        if(set1.has(e)) difference.delete(e)
+    });
+    return difference.size
+}
+
+console.log(a(set1,set2)+a(set2,set1))

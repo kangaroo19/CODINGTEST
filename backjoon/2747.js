f92be7beea22f4630ba333704ca8d8f8f1b26b85
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-07-01 18:44:57 +0900

    10826,2747

--- backjoon/2747.js
+++ backjoon/2747.js
@@ -1,5 +1,5 @@
 const fs=require('fs');
-const n=fs.readFileSync("example.txt").toString().trim().split('\n').map(v=>BigInt(v``));
+const n=fs.readFileSync("/dev/stdin").toString().trim().split('\n').map(v=>BigInt(v));
 const array=[]
 array[0]=0n,array[1]=1n,array[2]=1n
 for(let i=3;i<=n;i++){

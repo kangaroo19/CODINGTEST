d7e1f8ab761d30bbe66cc37d7263a4806c3b6f70
kangaroo19 <1000jjj@naver.com>
2022-08-03 02:14:24 +0900

    123

--- test.js
+++ test.js
@@ -16,23 +16,54 @@ class Stack{
 const fs=require('fs');
 const input=fs.readFileSync("example.txt").toString().trim().split('\n')
 
-const word=input.shift()
+let word=input.shift().trim()
 const n=Number(input.shift())
 const array=input
 
-let cur=word.length-2
+let cur=word.length-1
+let stack1=new Stack()
+
+
 //제출시 -1
 console.log(cur)
 array.forEach((element,index)=>{
     switch(element[0]){
         case 'L':
-            if(cur===-1) return
             cur--
+            if(cur<=-1) cur=-1
             break;
         case 'D':
             if(cur===word.length) return
             cur++
+            break;
+        case 'P':
+            let num=element.substring(2).trim()
+            if(cur!==-1){
+                for(let i=0;i<word.length;i++){
+                    stack1.push(word[i])
+                    if(i+1===cur+1){
+                        stack1.push(num)
+                    }
+                }
+            }
+            else{ //cur가 -1일때
+                for(let i=0;i<word.length;i++){
+                    stack1.push(word[i])
+                    if(i===cur+1){
+                        stack1.push(num)
+                    }
+                }
+            }
+            word=stack1.arr
+            stack1.arr=[]
+            stack1.index=0
+            cur++
+            break;
+        case 'B':
+            for(let i=0;i<word.length;i++){
+
+            }
     }
 })
 
-console.log(cur)
+console.log(word)

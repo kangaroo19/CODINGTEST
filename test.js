471ebfe272f37798a1c2dd75f978cc26f4776a2a
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-08-04 18:02:27 +0900

    123

--- test.js
+++ test.js
@@ -1,69 +1,14 @@
-class Stack{
-    constructor(){
-        this.arr=[]
-        this.index=0
-    }
-    push(item){
-        this.arr[this.index++]=item
-    }
-    pop(){
-        if(this.index<=0) return null
-        const result=this.arr[--this.index]
-        return result
-    }
-}
-
 const fs=require('fs');
-const input=fs.readFileSync("example.txt").toString().trim().split('\n')
+const input=fs.readFileSync("example.txt").toString().trim().split('\n');
+const nums = input.map(v => v.split(' ').map(v=>+v));
 
-let word=input.shift().trim()
-const n=Number(input.shift())
-const array=input
+const n=nums[0][0]
+const array=nums[1]
+const dp=[nums[1][0]]
 
-let cur=word.length-1
-let stack1=new Stack()
-
-
-//제출시 -1
-console.log(cur)
-array.forEach((element,index)=>{
-    switch(element[0]){
-        case 'L':
-            cur--
-            if(cur<=-1) cur=-1
-            break;
-        case 'D':
-            if(cur===word.length) return
-            cur++
-            break;
-        case 'P':
-            let num=element.substring(2).trim()
-            if(cur!==-1){
-                for(let i=0;i<word.length;i++){
-                    stack1.push(word[i])
-                    if(i+1===cur+1){
-                        stack1.push(num)
-                    }
-                }
-            }
-            else{ //cur가 -1일때
-                for(let i=0;i<word.length;i++){
-                    stack1.push(word[i])
-                    if(i===cur+1){
-                        stack1.push(num)
-                    }
-                }
-            }
-            word=stack1.arr
-            stack1.arr=[]
-            stack1.index=0
-            cur++
-            break;
-        case 'B':
-            for(let i=0;i<word.length;i++){
-
-            }
-    }
-})
-
-console.log(word)
+for(let i=1;i<n;i++){
+    dp[i]=array[i]>array[i]+dp[i-1]?array[i]:array[i]+dp[i-1]
+}
+//지금까지 더한 것보다 현재값(array안의 값)이 크면 현재값 dp에 넣음
+//아니라면 그대로 계속 더함
+console.log(Math.max(...dp))

a21b32f8419dab0b1839cb2979eb08871c58b1c6
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-08-11 21:09:13 +0900

    123

--- test.js
+++ test.js
@@ -1,20 +1,10 @@
 const fs=require('fs');
 const input=fs.readFileSync("example.txt").toString().trim().split(' ').map(v=>+v);
 let n=input[0]
-let k=input[1]
+const dp=[1,2,5]
 
-const arr=[0,[1],[1,1],[1,2,1]]
-
-for(let i=4;i<=n;i++){
-    arr[i]=[]
-    arr[i][0]=1
-    arr[i][i-1]=1
+for(let i=3;i<=n;i++){
     for(let j=0;j<i;j++){
-        if(j===0 || j===i-1){
-            continue
-        }
-        arr[i][j]=arr[i-1][j-1]+arr[i-1][j]
-        
+        dp[i]=dp[i-1]
     }
 }
-console.log(arr[n][k-1])

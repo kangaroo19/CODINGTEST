4bcf3df3259304f3a207094c62efe459dc773c8d
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-08-31 23:19:20 +0900

    123

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 const fs=require('fs');
 const input=fs.readFileSync("example.txt").toString().trim().split(' ').map(v=>+v);
-let n=input[0]
-const dp=[1,2,5]
 
-for(let i=3;i<=n;i++){
-    for(let j=0;j<i;j++){
-        dp[i]=dp[i-1]
-    }
-}
+const n=input.shift()
+const dp=[1,1,2]
+
--- test1.js
+++ test1.js
@@ -1,15 +1,8 @@
-const array=[5,4,3,2,1]
-const start=1
-const end=3
-
-let startSum=0
-let endSum=0
-for(let i=0;i<=end;i++){
-    endSum+=array[i]
+class Graph{
+    constructor(){
+        this.adjacencyList={}
+    }
+    addVertex(vertex){//정점 추가 메서드
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex]=[];
+    }
 }
-
-for(let i=0;i<=start-1;i++){
-    startSum+=array[i]
-}
-
-console.log(endSum-startSum)

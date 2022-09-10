319300b32fc7d224bdf71ab1f02ab476c78d4898
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-09-10 23:02:58 +0900

    123

--- test.js
+++ test.js
@@ -1,33 +1,90 @@
 const fs=require('fs');
 const input=fs.readFileSync("example.txt").toString().trim().split('\n');
-const n=Number(input.shift())
-const str=input.shift().trim()
-const word=str.split("")//후위표기식 저장
-const array=input //각 알파벳에 대응하는 값이 저장된 배열
-const stack=[]
 
-for(let i=0;i<word.length;i++){
-    if('A'<=word[i] && word[i]<='Z'){
-        word[i]=array[word[i].charCodeAt()-65].trim()
-    }   //65를 빼는 이유는 A의 아스키코드가 65이기 때문에
-        //각 위치에 대응하는 array의 값을 word에 넣기위함
-    stack.push(word[i])
-    if(word[i]==='+' || word[i]==='-' || word[i]==='/' || word[i]==='*'){
-        let op=stack.pop()
-        let b=stack.pop()
-        let a=stack.pop()
-        let temp='('+a+op+b+')'
-        stack.push(temp)
+for(let i=0;i<input.length-1;i++){
+    const word=input[i].trim().split("")
+    if(checkPossible(word)){
+        console.log(i+1+'. '+0)
+    }
+    else{
+        let count=0
+        let len=word.length
+        const lstack=[]
+        const rstack=[]
+        for(let i=0;i<len/2;i++){
+            lstack.push(word[i])
+        }
+        for(let i=word.length-1;i>=len/2;i--){
+            rstack.push(word[i])
+        }
+        let str=lstack.join("")+[...rstack].reverse().join("")
+        let j=0
+        while(1){
+            if(lstack[j]==='}' && rstack[j]==='{'){
+                let str2=checkMiddle(lstack,rstack,j)
+                if(checkPossible(str2)){
+                    console.log(123)
+                    j++
+                    continue
+                }
+                count+=2
+                lstack[j]='{'
+                rstack[j]='}'
+                str=lstack.join("")+[...rstack].reverse().join("")
+            }
+            else if(lstack[j]==='}' && rstack[j]==='}'){
+               
+                count+=1
+                lstack[j]='{'
+                str=lstack.join("")+[...rstack].reverse().join("")
+            }
+            else if(lstack[j]==='{' && rstack[j]==='{'){
+               
+                count+=1
+                rstack[j]='}'
+                str=lstack.join("")+[...rstack].reverse().join("")
+                
+                
+            }
+            j++
+            if(checkPossible(str)) break;
+        }
+        console.log(i+1+'. '+count)      
     }
 }
 
-console.log(eval(stack[0]).toFixed(2))
-//eval()함수는 문자열로된 수식을 계산해줌
-//처음엔 word변수가 배열이 아닌 string이었는데 각 요소에대한 수정이 되지않아
-//배열로 바꿈
 
-// let a='123'
-// a[0]='2'
-// console.log(a)
 
-//이것을 보면 a의 값은 223이 될거같지만 그렇지 않음
+
+function checkPossible(string){
+    const stack=[]
+    for(let i=0;i<string.length;i++){
+        let top=stack[stack.length-1]
+        const cur=string[i]
+        if(cur==='}' && top==='{'){
+            stack.pop()
+        }
+        
+        else{
+            stack.push(cur)
+        }
+    }
+    return stack.length?false:true
+}
+
+function checkMiddle(lstack,rstack,j){
+    let a=[]
+    let b=[]
+    lstack.filter((number,index,src)=>{
+        if(index<j){
+            a.push(number)
+        }
+    })
+    rstack.filter((number,index,src)=>{
+        if(index<j){
+            b.push(number)
+        }
+    })
+    let test=a.join("")+[...b].reverse().join("")
+    return test
+}
--- test1.js
+++ test1.js
@@ -1,3 +1,15 @@
-const str='123'
-const a=str.split("")
-console.log(a)
+const a=[1,2,3]
+const b=[6,5,4]
+const c=[]
+const d=[]
+a.filter((number,index,src)=>{
+    if(index<2)
+    c.push(number)
+})
+b.filter((number,index,src)=>{
+    if(index<2)
+    d.push(number)
+
+})
+
+console.log(c.join("")+d.reverse().join(""))

82b4787c99839940d2033c28813debd8d6e625b0
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-10-08 18:12:33 +0900

    123

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 const fs=require('fs');
-const input=fs.readFileSync("example.txt").toString().trim().split('\n').map(v=>+v);
+const input=fs.readFileSync("example.txt").toString().trim().split(' ');
 const n=input[0]
-const dp=new Array(n).fill(0)
-dp[1]=input[1]
-dp[2]=input[1]+input[2]
-
-for(let i=3;i<=n;i++){
-    dp[i]=Math.max(dp[i-3]+input[i-1]+input[i],dp[i-2]+input[i],dp[i-1])
+const dp=[0,[1,1,1,1,1,1,1,1,1,1]]
+for(let i=2;i<=n;i++){
+    dp[i]=[]
+    dp[i][0]=dp[i-1][0]%10007
+    for(let j=1;j<=10;j++){
+        dp[i][j]=(dp[i][j-1]+dp[i][j])%10007
+    }
 }
-console.log(dp[n])
-
-//첫번째 경우 => dp[n-3]이후에 n-2거르고 n-1,n 선택
-//두번째 경우 => dp[n-2]이후에 n 선택
-//세번째 경우 => dp[n-1]만
+console.log(dp[n].reduce((a,c)=>a+c)%10007)
+// let hap=0
+// for(let i=1;i<=1;i++){
+//     hap+=i
+// }
+// console.log(55+45+36+28+21+15+10+6+3+1)

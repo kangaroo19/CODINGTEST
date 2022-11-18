820689d8ffe4b24d7918871c948e845e2393bde9
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-11-18 14:06:17 +0900

    123

--- test1.js
+++ test1.js
@@ -1,6 +1,6 @@
-const dp=[[1,2,3,4,5],
-          [6,7,8,9,2],
-          [2,4,6,7,2]]
-let i=0
-console.log(dp[0][-1]||0)
-const arr=[[1,2,3,4,5,6]]
+let l0='l0'
+function fn1(){
+    let l1='l1'
+    console.log(l0,l1)
+}
+fn1()

00ee5adf66449d3d913b929b3d52d80b23233fd9
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-11-30 19:44:49 +0900

    123

--- test.js
+++ test.js
@@ -1,7 +1,49 @@
-const arr=[{'R':0,'T':0},{'C':0,'f':0},{'J':0,'M':0},{'A':0,'N':0}]
-const r='R'
-arr.forEach((v,i)=>{
-    console.log(v,i)
-})
 
-console.log(arr[0].r)
+function solution1(n) {
+    const AntExp = /(.)\1*/g;
+    let start = '1'; 
+    
+    for (let i = 1; i <n; i++) {
+        let str = ''; 
+        const result = start.match(AntExp);
+        console.log(result)
+        for (item of result) {
+            str += String(item.length) + item[0]; 
+        }
+        start = str;
+        console.log(start.split('').reverse().join('')) 
+    }
+    return start.split('').reverse().join('')
+}
+
+//console.log(solution1(5))
+
+const testExp=/(.)\1*/g
+const start='11223232'
+const result=start.match(testExp)
+
+// console.log(result)
+
+
+
+function solution2(n){
+    const a='4'
+    const b='13'
+    const array1=[]
+    const array2=[]
+    let answer=0
+    let cnt=0
+    while(cnt<=n){
+        let num=array1.splice(0,1)
+        //console.log('num '+num)
+        array1.push(parseInt(num+a))
+        array1.push(parseInt(num+b))
+        //console.log(array1)
+        array2.push(num)
+        //console.log(array2)
+        cnt++
+    }
+    return array2.sort((a,b)=>a-b)
+}
+solution2(1000)
+

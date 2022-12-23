1cfa7089303cf3ceedf472e5bb2bc6abecdb58a8
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-12-23 18:21:26 +0900

    123

--- programmers/영어_끝말잇기.js
+++ programmers/영어_끝말잇기.js
@@ -43,11 +43,10 @@ function solution2(n, words) {
       let word = words[i];
       let p = i % n + 1;
       let turn = Math.ceil((i + 1)/n);
-        
+
       if (i > 0) {
         // 이전 단어 마지막 글자
         let last = words[i - 1].split("").pop();
-        console.log(last)
         // 중복이거나 틀린 경우
         if (i > words.indexOf(word) || words[i][0] !== last) {
           answer = [p, turn];
--- test.js
+++ test.js
@@ -1,32 +1,9 @@
-const n=2
-// const words=["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
-//const words=["hello", "one", "even", "never", "now", "world", "draw"]
-//const words=["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]
-const words=['aa','aba','aba','aa']
-const stack=[words[0]]
-for(let i=1;i<words.length;i++){
-    if(stack[i-1][stack[i-1].length-1]===words[i][0]){
-        stack.push(words[i])
-    }
-    else if(words[i].length===1){
-        stack.push(false)
-        break
-    }
-    else{
-        stack.push(false)
-        break
-    }
-    for(let j=0;j<stack.length-1;j++){
-        if(words[i]===stack[j]){
-            stack.pop()
-            stack.push(false)
-            break
-        }
-    }
+const t="3141592"
+const p='271'
+let count=0
+for(let i=0;i<t.length-p.length+1;i++){
+    let subStr=t.substring(i,i+p.length)
+    if(Number(p)>=Number(subStr)) count++
 }
-for(let i=0;i<stack.length;i++){
-    if(stack[i]===false){
-        console.log([i%n+1,Math.floor(i/n+1)])
-        break;
-    }
-}
+
+console.log(count)

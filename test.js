0297b2067ba2db1701ba01b00db2517a4e32a2c8
kangaroo19 <1000jjj@naver.com>
2024-05-27 18:16:14 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,8 @@
-let n = 10;
-let numbers = new Array();
-// const numbers=new Array(10000000)
-for (let i = 1; i <= n; i++) {
-  numbers.push(i);
+var age=25
+
+if(true){
+  let korAge=age+1
+  console.log('my age is '+korAge)
 }
 
-
-// console.time('For in')
-// for(let i in numbers){}
-// console.timeEnd('For in')
-
-// console.time('for of')
-// for(let i of numbers){}
-// console.timeEnd('for of')
-
-// console.time('forEach')
-// numbers.forEach((v,i)=>{})
-// console.timeEnd('forEach')
-
-// console.time('for')
-// for(let i;i<numbers.length;i++){}
-// console.timeEnd('for')
-
-
-
-let iterable = new Uint8Array([0x00, 0xff, 0xff]);
-for(i of iterable){
-  console.log(i)
-}
+console.log(korAge)
